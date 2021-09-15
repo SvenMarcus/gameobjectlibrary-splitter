682df1f2a90ec4cdd31a7d7526564458ca51d1a2
Sven Marcus <s.marcus@outlook.de>
2021-09-15 16:49:58 +0200

    Even better pattern matching

--- split_gameobjectlibrary.lua
+++ split_gameobjectlibrary.lua
@@ -6,18 +6,31 @@ local function replace_comparator_strings(entry)
         "LessOrEqualTo", "IsOneOf", "InInterval"
     }
 
-    for i, comparator in ipairs(comparators) do
-        entry = string.gsub(entry, "\""..comparator .. "%({?[%w,?%s]*}?%)".."\"", function(str)
-            return string.gsub(str, "\"", "")
+    for _, comparator in ipairs(comparators) do
+        entry = string.gsub(entry, "\"" .. comparator ..
+                                "%({?[%w,?%s]*}?%)" .. "\"",
+                            function(str)
+            str = string.gsub(str, "%[?\"", "")
+            str = string.gsub(str, "%]?", "")
+            return str
         end)
     end
 
     return entry
 end
 
+local function replace_string_assignments(entry)
+    return string.gsub(entry, "%[\"[%w_?]*\"[%]]?", function(str)
+        str = string.gsub(str, "%[?\"", "")
+        str = string.gsub(str, "%]?", "")
+        return str
+    end)
+end
+
 local function write_entry_file(entry_name, entry)
     local file = io.open("split_files/" .. entry_name .. ".lua", "w")
     local entry_string = serializer:serialize(entry)
+    entry_string = replace_string_assignments(entry_string)
     entry_string = replace_comparator_strings(entry_string)
     file:write(entry_string)
     file:close()
@@ -27,9 +40,7 @@ local function split_game_object_library()
     local game_object_lib = require("GameObjectLibrary")
 
     os.execute("mkdir -p split_files")
-    for k, v in pairs(game_object_lib.Units) do
-        write_entry_file(k, v)
-    end
+    for k, v in pairs(game_object_lib.Units) do write_entry_file(k, v) end
 end
 
 split_game_object_library()
